5d6e8da50b3c0c35cc6324bde720b25295ae0d94
rushannanayakkara <rushannana@gmail.com>
2025-02-26 07:48:55 +0530

    Add app native authentication support

--- components/org.wso2.carbon.identity.application.authenticator.kakao/src/main/java/org/wso2/carbon/identity/application/authenticator/kakao/KakaoCustomAuthenticatorConstants.java
+++ components/org.wso2.carbon.identity.application.authenticator.kakao/src/main/java/org/wso2/carbon/identity/application/authenticator/kakao/KakaoCustomAuthenticatorConstants.java
@@ -21,12 +21,73 @@ package org.wso2.carbon.identity.application.authenticator.kakao;
 public class KakaoCustomAuthenticatorConstants {
 
     public static final String AUTHENTICATOR_NAME = "KAKAO";
+    public static final String AUTHENTICATOR_I18N_KEY = "authenticator.kakao";
     public static final String AUTHENTICATOR_FRIENDLY_NAME = "KAKAO";
+    public static final String ACCESS_TOKEN_PARAM = "accessToken";
+    public static final String AUTHENTICATOR_MESSAGE = "authenticatorMessage";
+    public static final String ID_TOKEN_PARAM = "idToken";
+    public static final String STATE = "state";
     public static final String CLIENT_ID = "ClientId";
     public static final String CLIENT_SECRET = "ClientSecret";
     public static final String CALLBACK_URL = "CallbackUrl";
+    public static final String REDIRECT_URL = "redirectUrl";
     public static final String KAKAO_AUTH_URL = "https://kauth.kakao.com/oauth/authorize";
     public static final String KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
     public static final String KAKAO_INFO_URL = "https://kapi.kakao.com/v2/user/me";
+    public static final String KAKAO_OAUTH2_STATE_SUFFIX = ",oauth2";
+    
+    
+    private KakaoCustomAuthenticatorConstants() {
+    }
+
+
+
+    public enum ErrorMessages {
+
+        NO_REGISTERED_IDP_FOR_ISSUER("Kakao-65001", "No registered IdP found for the issuer: %s"),
+        JWT_TOKEN_VALIDATION_FAILED("Kakao-65002", "Error while validating the ID token."),
+        ID_TOKEN_AUD_VALIDATION_FAILED("Kakao-65003", "Invalid audience in the ID token.");
+
+        private final String code;
+        private final String message;
+
+        /**
+         * Create an Error Message.
+         *
+         * @param code    Relevant error code.
+         * @param message Relevant error message.
+         */
+        ErrorMessages(String code, String message) {
+
+            this.code = code;
+            this.message = message;
+        }
+
+        /**
+         * To get the code of specific error.
+         *
+         * @return Error code.
+         */
+        public String getCode() {
+
+            return code;
+        }
+
+        /**
+         * To get the message of specific error.
+         *
+         * @return Error message.
+         */
+        public String getMessage() {
+
+            return message;
+        }
+
+        @Override
+        public String toString() {
+
+            return String.format("%s  - %s", code, message);
+        }
+    }
 }
 
