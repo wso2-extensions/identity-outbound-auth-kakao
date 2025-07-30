a485fa9f69d8a9cee45a71990d6af722d8215fc3
firesprite <firesprite@empal.com>
2025-07-30 15:48:29 +0900

    Update KakaoCustomAuthenticatorConstants.java
    add static variable  USER_ID

--- components/org.wso2.carbon.identity.application.authenticator.kakao/src/main/java/org/wso2/carbon/identity/application/authenticator/kakao/KakaoCustomAuthenticatorConstants.java
+++ components/org.wso2.carbon.identity.application.authenticator.kakao/src/main/java/org/wso2/carbon/identity/application/authenticator/kakao/KakaoCustomAuthenticatorConstants.java
@@ -29,6 +29,7 @@ public class KakaoCustomAuthenticatorConstants {
     public static final String KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
     public static final String KAKAO_INFO_URL = "https://kapi.kakao.com/v2/user/me";
     public static final String KAKAO_OAUTH2_STATE_SUFFIX = ",oauth2";
+    public static final String USER_ID = "id";
 
     private KakaoCustomAuthenticatorConstants() {
     }
